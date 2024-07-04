f52307746bb88c1539d821d9831a3968c82cb0c8
Daniel24356 <igwedaniel128@gmail.com>
2024-07-04 14:56:34 -0700

    Description of changes

---
+++ src/components/footer.tsx
@@ -0,0 +1,32 @@
+ import './footer.css'
+
+ const Footer = () => {
+    return (
+        <>
+        <footer>
+            <div className='gray-footer'>
+             <p>Latest Buyer’s Guides</p>
+             <div>
+             <li>How to choose your telescope magnification?</li>
+             <li>Best Space (Astronomy) Hoodies for Kids</li>
+             <li>Best Space T-Shirts for Kids</li>
+             <li>Best High Power Binoculars for Experts</li>
+             <li>Best Travel Telescopes</li>
+             <li>Choosing Your Astronomy Equipment</li>
+             <li>Best Space T-Shirts Adults</li>
+             <li>Best Astronomy Gifts under $100</li>
+             <li>Monocular vs. Binoculars- Which</li>
+             <li>One is Best for Stargazing</li>
+             </div>
+
+            </div>
+            <div className='privacy-footer'>
+            <p>© The Nine Planets 1994-2024</p>
+            <p>Privacy Policy | Terms & Conditions | Cookies Policy | Affiliate Disclosure</p>
+            </div>
+        </footer>
+        </>
+    )
+ }
+
+ export default Footer
