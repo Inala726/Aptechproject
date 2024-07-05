78b6b991d8e7f0b54e35a52794689089e0fb49be
That0neboiii <walterase300@gmail.com>
2024-07-05 22:31:23 +0100

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import "./App.css";
 import Herosection from "./components/Herosection";
 import Course from './components/Course'
 import Footer from './components/footer'
+import PlanetSection from './components/PlanetSection/PlanetSection';
+import './App.css';
+
     
 function App() {
   return (
@@ -9,6 +12,7 @@ function App() {
   <Herosection heading={"The Nine Planets"} bracket={"We still love you, Pluto!"} paragraph={"An overview of the history, mythology and current scientific knowledge of the planets, moons and other objects in our solar system. "}/>  
   <Course/>
   <Footer/>
+  <PlanetSection />
     </>
       );
 }
